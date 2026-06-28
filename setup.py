a6a6049f1f546784c1a1140aa9d27542336f7134
abhishek14codes <dhingraabhishek514@gmail.com>
2026-06-28 13:22:13 +0530

    requirements change

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def get_requirements(file_path:str) -> List[str] :
     requirements=[]
     with open(file_path) as file_obj :
         requirements = file_obj.readlines()
-        requirements=[req.replace('/n' , ' ') for req in requirements]
+        requirements=[req.strip() for req in requirements]
         if hyp_e in requirements :
             requirements.remove(hyp_e)
         return requirements        
